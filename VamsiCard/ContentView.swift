1c75fba3092cf945ec995b3c97537527eb4bc44c
vamsi <vamsikrishnayadav2002@gmail.com>
2022-08-08 20:13:11 +0530

    Initial Commit

---
+++ VamsiCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VamsiCard
+//
+//  Created by Mekala Vamsi Krishna on 08/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VamsiCard/VamsiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VamsiCardApp.swift
+//  VamsiCard
+//
+//  Created by Mekala Vamsi Krishna on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct VamsiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
