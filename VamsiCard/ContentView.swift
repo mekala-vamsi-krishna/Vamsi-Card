85896c50fa1ea00221100f5a2a559a0fd39de5bc
 Mekala Vamsi Krishna <vamsikrishnayadav2002@gmail.com>
2022-08-12 21:25:32 +0530

    Complete VamsiCard app

--- VamsiCard/ContentView.swift
+++ VamsiCard/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("vamsi")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Mekala Vamsi Krishna")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+91 7995684057", imageName: "phone.fill")
+                InfoView(text: "vamsikrishnayadav2002@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
@@ -19,3 +41,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
---
+++ VamsiCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  VamsiCard
+//
+//  Created by Mekala Vamsi Krishna on 08/08/22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(Color(red: 0.09, green: 0.63, blue: 0.52))
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
